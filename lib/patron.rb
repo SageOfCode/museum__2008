ef26e0c0254de570fb242c6371bcfd4b330911da
Sage Gonzales <sagegonzales15@gmail.com>
2020-09-10 09:18:43 -0600

    passes it knows interests test

--- lib/patron.rb
+++ lib/patron.rb
@@ -3,5 +3,14 @@ class Patron
   def initialize(name, spending_money)
     @name = name
     @spending_money = spending_money
+    @interests = []
+  end
+
+  def add_interest(interest)
+    @interests << interest
+  end
+
+  def interests
+    @interests
   end
 end
