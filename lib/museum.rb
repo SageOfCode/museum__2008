e0a53baa287c18fb814eab44498388e47c1cf343
Sage Gonzales <sagegonzales15@gmail.com>
2020-09-10 10:03:27 -0600

    passes it can recommend based on interests test

--- lib/museum.rb
+++ lib/museum.rb
@@ -10,6 +10,18 @@ class Museum
   end
 
   def add_exhibit(exhibit)
-    @exhibits << exhibit 
+    @exhibits << exhibit
+  end
+
+  def recommend_exhibits(patron)
+    interest_list = []
+    @exhibits.find_all do |exhibit|
+      patron.interests.each do |interest|
+        if exhibit.name == interest
+          interest_list << exhibit
+        end
+      end
+    end
+    interest_list.reverse
   end
 end
