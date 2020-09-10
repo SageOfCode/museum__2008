d6fa1184ce941652f6f411965b2a796fb9c4c83b
Sage Gonzales <sagegonzales15@gmail.com>
2020-09-10 09:28:18 -0600

    passes it can add exhibits test

--- lib/museum.rb
+++ lib/museum.rb
@@ -2,5 +2,14 @@ class Museum
   attr_reader :name
   def initialize(name)
     @name = name
+    @exhibits = []
+  end
+
+  def exhibits
+    @exhibits
+  end
+
+  def add_exhibit(exhibit)
+    @exhibits << exhibit 
   end
 end
